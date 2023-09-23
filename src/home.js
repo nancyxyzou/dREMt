e1324319b5703e7b10e587fbe45ae66acc94590e
Yaxin <113393972+yaxinw04@users.noreply.github.com>
2023-09-23 17:54:20 -0400

    yay

--- src/home.js
+++ src/home.js
@@ -5,26 +5,39 @@ const DreamInterpreter = () => {
   const [interpretation, setInterpretation] = useState("");
 
   const handleSubmit = async () => {
-    const prompt = `Interpret the following dream: ${dream}`;
+    const prompt = `this was my dream: ${dream}, can u interpret it and tell me what it means?`;
 
-    const response = await fetch(
-      "https://api.openai.com/v1/engines/davinci/completions",
-      {
-        method: "POST",
-        headers: {
-          Authorization:
-            "Bearer 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",
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt: prompt, max_tokens: 150 }),
-      }
-    );
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization:
+          "Bearer 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",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content:
+              "You are a dream interpreter that helps people make sense of their dreams. Do not exceed 3-4 sentences in your response.",
+          },
+          {
+            role: "user",
+            content: prompt,
+          },
+        ],
+        max_tokens: 200,
+      }),
+    });
 
     const data = await response.json();
 
     console.log(data);
     if (data.choices && data.choices.length > 0) {
-      setInterpretation(data.choices[0].text.trim());
+      const responseData = data.choices[0].message.content.trim();
+      const interpretationResponse = responseData.split("\n").pop();
+      setInterpretation(interpretationResponse);
     } else {
       console.error("No interpretation received from the API.");
       setInterpretation("Unable to interpret the dream.");
